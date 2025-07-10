1d5777048ec26343f6c9149208f139ef2c693a96
technowin <technowinitinfra@gmail.com>
2025-07-10 15:27:59 +0530

    fdfdf

--- Workflow/models.py
+++ Workflow/models.py
@@ -145,3 +145,18 @@ class ReferenceFormStatus(models.Model):
         db_table = 'reference_form_status'
 
 
+class client_module (models.Model):
+    client = models.TextField(null=True, blank=True)
+    department = models.TextField(null=True, blank=True)
+    module = models.TextField(null=True, blank=True)
+    status = models.IntegerField(null=True, blank=True)
+    created_at = models.DateTimeField(null=True, blank=True)             
+    created_by = models.TextField(null=True, blank=True) 
+    updated_at = models.DateTimeField(null=True, blank=True)               
+    updated_by = models.TextField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'client_module'
+
+    def __str__(self):
+        return f"{self.client} - {self.department} - {self.module}"
