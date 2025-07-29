b51db19647dbd2d36b1d4638e8ef287ed3ad6bc0
technowin <technowinitinfra@gmail.com>
2025-07-29 17:51:54 +0530

    fhghg

--- TWX/settings.py
+++ TWX/settings.py
@@ -149,7 +149,8 @@ LOCAL_APPS = [
     'Form',
     'BOM',
     'MaterialPlan',
-    'MachinePlan'
+    'MachinePlan',
+    'BookMetadata'
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
